be748f265ad526455bcbb3803610dce6423331e4
onmedicines <semwalanurag26@gmail.com>
2025-02-13 00:04:34 +0530

    fix - ham menu correctly takes up the required srceen

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   const [isMenuVisible, setMenuVisible] = useState(false);
 
   return (
-    <div className={`max-w-screen min-h-screen overflow-x-hidden bg-slate-100 ${isMenuVisible ? "flex flex-col max-h-screen" : ""}}`}>
+    <div className={`max-w-screen min-h-screen overflow-x-hidden bg-slate-400 flex flex-col ${isMenuVisible ? "max-h-screen" : ""} }`}>
       <nav className="flex justify-between items-center py-4 px-4">
         {isMenuVisible ? (
           <X
@@ -28,25 +28,25 @@ function Home() {
         </Link>
         <User />
       </nav>
-      {/* Nav menu */}
-      <div className={`w-full h-full bg-blue-400 transition-transform duration-300 flex-grow flex flex-col justify-between p-4 ${isMenuVisible ? "transform translate-x-0" : "transform -translate-x-full"}`}>
-        <ul className="flex flex-col gap-6 list-disk py-8 px-4">
-          <Link to="/">
-            <li className="text-2xl text-slate-200 font-bold">Home</li>
+      <main className={`relative bg-amber-200 flex-grow`}>
+        {/* Nav menu */}
+        <div className={`absolute z-10 w-full h-full bg-blue-400 transition-transform duration-300 flex flex-col justify-between p-4 ${isMenuVisible ? "transform translate-x-0" : "transform -translate-x-full"}`}>
+          <ul className="flex flex-col gap-6 list-disk py-8 px-4">
+            <Link to="/">
+              <li className="text-2xl text-slate-200 font-bold">Home</li>
+            </Link>
+            <Link to="/">
+              <li className="text-2xl text-slate-200 font-bold">Home</li>
+            </Link>
+            <Link to="/">
+              <li className="text-2xl text-slate-200 font-bold">Home</li>
+            </Link>
+          </ul>
+          <Link to={"/"} className="py-2 text-center w-full rounded-sm bg-orange-400 text-slate-200 text-xl font-bold">
+            Feedback
           </Link>
-          <Link to="/">
-            <li className="text-2xl text-slate-200 font-bold">Home</li>
-          </Link>
-          <Link to="/">
-            <li className="text-2xl text-slate-200 font-bold">Home</li>
-          </Link>
-        </ul>
-        <Link to={"/"} className="py-2 text-center w-full rounded-sm bg-orange-400 text-slate-200 text-xl font-bold">
-          Feedback
-        </Link>
-      </div>
-      {/* Nav menu */}
-      <main>
+        </div>
+        {/* Nav menu */}
         <BookFlight />
       </main>
     </div>
